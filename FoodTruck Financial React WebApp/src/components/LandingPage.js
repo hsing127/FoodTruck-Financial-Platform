9d7bf7adde3ce69868c5c015a32e396d95438455
Roshan Arun <96106273+RoshanArun@users.noreply.github.com>
2024-10-03 13:09:08 -0700

    Created Landing Page
    Developed Simple landing page, has basic information as well  as company logo and appropriate gradient

--- FoodTruck Financial React WebApp/src/components/LandingPage.js
+++ FoodTruck Financial React WebApp/src/components/LandingPage.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import '../styles/LandingPage.css'; 
+import logo from '../assets/Logo.png'; 
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
+    <div className="landing-container">
+      <div className="brand-logo">
+        <img src={logo} alt="FoodTrack Logo" className="logo-image" />
+        <h2>FoodTrack</h2>
+      </div>
+
+      <div className="intro-section">
+        <p className="intro-badge">Introducing FoodTrack</p>
+        <h1>Optimize your food truck's financials</h1>
+        <p className="intro-description">
+          Manage and analyze your expenses, automate receipt processing, and maximize your profits with insights tailored for food truck owners.
+        </p>
+        <button className="cta-button">Get started</button>
+      </div>
+      
     </div>
   );
 };
