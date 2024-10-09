b4bb655972cc9011f036131984c5ab3a29d18861
Roshan Arun <96106273+RoshanArun@users.noreply.github.com>
2024-10-09 15:28:41 -0700

    Spacing issues

--- FoodTruck Financial React WebApp/src/components/landingPage/LogoTicker.tsx
+++ FoodTruck Financial React WebApp/src/components/landingPage/LogoTicker.tsx
@@ -15,7 +15,9 @@ export const LogoTicker = () => {
       <div className="container">
         <h2 className="text-xl text-center text-white/70">Sponsored by Food Truck Association Of Canada</h2>
         <h2 className="text-lg text-center text-white/70">How we support the industry:</h2>
-        <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
+        <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full 
+        after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 
+        before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
           <div className="flex gap-16 justify-center">
             {texts.map((text, index) => (
               <div key={index} className="flex-none text-center">
