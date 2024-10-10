a07674bab0356e3b11973d6cade542b17b1635e0
Alex Witt <alexwitt0@gmail.com>
2024-10-10 04:47:07 -0700

    Creation of Main Dashboard page
    Created main dashboard page, adjusted some sidebar coloring which requires fixes for the financial pages. Will continue to fix spacing, color scheme, and pages affected by changes.

---
+++ FoodTruck Financial React WebApp/src/components/dashboard/DashboardMenu.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+// const DashboardMenuPage: React.FC = () => {
+//   return (
+//     <div>
+//       <h1>Menu Dashboard</h1>
+//     </div>
+//   );
+// };
+
+// export default DashboardMenuPage;
--- FoodTruck Financial React WebApp/src/pages/dashboard/menu.tsx
+++ FoodTruck Financial React WebApp/src/pages/dashboard/menu.tsx
@@ -1,4 +1,4 @@
-import DashboardMenuPage from '../../components/dashboard/DashboardMenuPage';
+import DashboardMenuPage from '../../components/dashboard/DashboardMainMenu';
 
 const Page: React.FC = () => {
   return <DashboardMenuPage />;
