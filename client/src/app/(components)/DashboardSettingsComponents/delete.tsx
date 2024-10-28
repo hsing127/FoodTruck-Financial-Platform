13ac95d633338518a1de2a8b54a7574f8c59b367
Roshan Arun <96106273+RoshanArun@users.noreply.github.com>
2024-10-28 12:40:48 -0700

    Update delete.tsx

--- client/src/app/(components)/DashboardSettingsComponents/delete.tsx
+++ client/src/app/(components)/DashboardSettingsComponents/delete.tsx
@@ -7,7 +7,7 @@ const DeleteAccount: React.FC = () => {
     <SectionCard icon={Trash2} title="Delete Account">
       <div className="text-gray-700">
         <p className="mb-4">Permanently delete your account. This action cannot be undone.</p>
-        <button className="bg-[#8B5CF6] hover:bg-[#b07ff0] text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto">
+        <button className="bg-red-400 hover:bg-red-300 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto">
           Delete Account
         </button>
       </div>
