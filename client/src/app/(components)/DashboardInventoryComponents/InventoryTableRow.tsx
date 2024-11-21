918006917a6af4a264abe2da649cc199f82e259c
Wenjie Xu <indoxwj@gmail.com>
2024-11-21 09:18:30 -0700

    Use updateTable API to allow users to update the DB ingredients table from the inventory page.

--- client/src/app/(components)/DashboardInventoryComponents/InventoryTableRow.tsx
+++ client/src/app/(components)/DashboardInventoryComponents/InventoryTableRow.tsx
@@ -5,6 +5,7 @@ import ActionButtons from "../Common/ActionButtons";
 import { InventoryItem } from "@/app/types/types";
 import { useEditable } from "@/app/hooks/useEditable";
 import { tableRowTransition } from "../Common/Animations";
+import { useEditInventoryData } from "./InventoryApi";
 
 interface InventoryTableRowProps {
   item: InventoryItem;
@@ -20,80 +21,81 @@ const rowVariants = {
 };
 
 const InventoryTableRow: React.FC<InventoryTableRowProps> = ({
-  item,
-  index,
-  onItemEdit,
-  onItemDelete,
-  setIsAnimating,
-}) => {
-  const {
-    isEditing,
-    editedItem,
-    handleEditClick,
-    handleSaveClick,
-    handleCancelClick,
-    handleInputChange,
-  } = useEditable<InventoryItem>(item);
-
-  const handleSave = (e: React.MouseEvent) => {
-    handleSaveClick();
-    onItemEdit(item.id, editedItem);
+    item,
+    index,
+    onItemEdit,
+    onItemDelete,
+    setIsAnimating,
+  }) => {
+    const {
+      isEditing,
+      editedItem,
+      handleEditClick,
+      handleSaveClick,
+      handleCancelClick,
+      handleInputChange,
+    } = useEditable<InventoryItem>(item);
+  
+    const { editInventory, loading } = useEditInventoryData();
+  
+    const handleSave = async (e: React.MouseEvent) => {
+      e.stopPropagation();
+      handleSaveClick();
+      await editInventory(item, editedItem, "ajwitt2@asu.edu");
+      onItemEdit(item.id, editedItem);
+    };
+  
+    return (
+      <motion.tr
+        variants={rowVariants}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        transition={{
+          delay: index * 0.03,
+          ...tableRowTransition,
+          onStart: () => setIsAnimating(true),
+          onComplete: () => setIsAnimating(false),
+        }}
+        className="cursor-pointer border-b border-t border-white"
+      >
+        <EditableCell
+          isEditing={false}
+          value={item.Name}
+          name="Name"
+          onChange={() => {}}
+        />
+        <EditableCell
+          isEditing={isEditing}
+          value={isEditing ? editedItem.Amount : item.Amount}
+          name="Amount"
+          onChange={handleInputChange}
+          type="number"
+        />
+        <EditableCell
+          isEditing={isEditing}
+          value={isEditing ? editedItem.AmountUnits : item.AmountUnits}
+          name="AmountUnits"
+          onChange={handleInputChange}
+        />
+        <ActionButtons
+          isEditing={isEditing}
+          onEdit={(e) => {
+            e.stopPropagation();
+            handleEditClick();
+          }}
+          onSave={handleSave}
+          onCancel={(e) => {
+            e.stopPropagation();
+            handleCancelClick();
+          }}
+          onDelete={(e) => {
+            e.stopPropagation();
+            onItemDelete(item.id); // Use 'id' as the identifier
+          }}
+        />
+      </motion.tr>
+    );
   };
-
-  return (
-    <motion.tr
-      variants={rowVariants}
-      initial="hidden"
-      animate="visible"
-      exit="hidden"
-      transition={{
-        delay: index * 0.03,
-        ...tableRowTransition,
-        onStart: () => setIsAnimating(true),
-        onComplete: () => setIsAnimating(false),
-      }}
-      className="cursor-pointer border-b border-t border-white"
-    >
-      <EditableCell
-        isEditing={false}
-        value={item.Name}
-        name="Name"
-        onChange={() => {}}
-      />
-      <EditableCell
-        isEditing={isEditing}
-        value={isEditing ? editedItem.Amount : item.Amount}
-        name="Amount"
-        onChange={handleInputChange}
-        type="number"
-      />
-      <EditableCell
-        isEditing={isEditing}
-        value={isEditing ? editedItem.AmountUnits : item.AmountUnits}
-        name="AmountUnits"
-        onChange={handleInputChange}
-      />
-      <ActionButtons
-        isEditing={isEditing}
-        onEdit={(e) => {
-          e.stopPropagation();
-          handleEditClick();
-        }}
-        onSave={(e) => {
-          e.stopPropagation();
-          handleSave(e);
-        }}
-        onCancel={(e) => {
-          e.stopPropagation();
-          handleCancelClick();
-        }}
-        onDelete={(e) => {
-          e.stopPropagation();
-          onItemDelete(item.id); // Use 'id' as the identifier
-        }}
-      />
-    </motion.tr>
-  );
-};
-
-export default InventoryTableRow;
+  
+  export default InventoryTableRow;
