4f40d9f172f7fc35db08c37183099c9963d73a44
Ryan Gaudier <113002432+RyanGudi@users.noreply.github.com>
2024-11-22 23:11:16 -0700

    API edit function implementation

--- client/src/app/(components)/DashboardMenuComponents/MenuAPI.tsx
+++ client/src/app/(components)/DashboardMenuComponents/MenuAPI.tsx
@@ -7,6 +7,122 @@ export const useMenuData = (email: string) => {
   const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
+  //Function to edit menu item
+  const editIngredientItemAPI = async (email: String, newMenuItem: MenuItem, oldMenuItem: MenuItem, newIngredientItem: Ingredient, oldIngredientItem: Ingredient) => {
+    try {
+      const response = await fetch(
+        "https://y4frxnym9g.execute-api.ca-central-1.amazonaws.com/dev/data/editTable",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            table: "uses",
+            body: [
+              {
+                Email: email,
+                NewMenuName: newMenuItem.name,
+                NewIngredientName: newIngredientItem.ingredient,
+                NewAmount: newIngredientItem.price,
+                NewAmountUnits: newIngredientItem.quantity,
+                MenuName: oldMenuItem.name,
+                IngredientName: oldIngredientItem.ingredient
+              },
+            ],
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to update menu item in API");
+      }
+
+      const data = await response.json();
+      console.log("Menu item updated successfully:", data);
+      return data;
+    } catch (error) {
+      console.error("Error updating menu item:", error);
+      throw new Error("Failed to update menu item to API");
+    }
+  }
+
+
+  //Function to edit menu item
+  const editMenuItemAPI = async (email: String, newMenuItem: MenuItem, oldMenuItem: MenuItem) => {
+    try {
+      const response = await fetch(
+        "https://y4frxnym9g.execute-api.ca-central-1.amazonaws.com/dev/data/editTable",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            table: "menuItem",
+            body: [
+              {
+                Email: email,
+                NewName: newMenuItem.name,
+                NewCost: newMenuItem.price,
+                Name: oldMenuItem.name
+              },
+            ],
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to update menu item in API");
+      }
+
+      const data = await response.json();
+      console.log("Menu item updated successfully:", data);
+      return data;
+    } catch (error) {
+      console.error("Error updating menu item:", error);
+      throw new Error("Failed to update menu item to API");
+    }
+  }
+
+  //Function to send ingredient to API Gateway
+  const sendIngredientToAPI = async (email: String, menuItem: MenuItem, ingredient: Ingredient) => {
+    try {
+      const response = await fetch(
+        "https://y4frxnym9g.execute-api.ca-central-1.amazonaws.com/dev/data/addData",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            table: "uses",
+            body: [
+              {
+                Email: email,
+                MenuName: menuItem.name,
+                IngredientName: ingredient.ingredient,
+                Amount: ingredient.price,
+                AmountUnits: ingredient.quantity
+              },
+            ],
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to add ingredient item to API");
+      }
+
+      const data = await response.json();
+      console.log("Ingredient item added successfully:", data);
+      return data;
+    } catch (error) {
+      console.error("Error adding ingredient item:", error);
+      throw new Error("Failed to add ingredient item to API");
+    }
+  };
+
   //Function to send menu item to API Gateway
   const sendMenuItemToAPI = async (email: String, menuItem: MenuItem) => {
     try {
@@ -120,5 +236,8 @@ export const useMenuData = (email: string) => {
     loading,
     addMenuItem,
     sendMenuItemToAPI,
+    sendIngredientToAPI,
+    editMenuItemAPI,
+    editIngredientItemAPI
   ] as const;
 };
